4af8c7e80fbce1219b104c2209e7cbb6a9ebd0b6
Chengkai Yang <jenson.yang20@gmail.com>
2019-10-06 13:19:40 -0400

    added cp_info

--- miniprogram/pages/cp_info_display/cp_info.js
+++ miniprogram/pages/cp_info_display/cp_info.js
@@ -1,66 +1,50 @@
-// miniprogram/pages/cp_info_display/cp_info.js
+//https://cloud.tencent.com/developer/article/1380912
+
+const db = wx.cloud.database({});
+const cont = db.collection('user');
+const app = getApp()
+var openid = undefined;
+var user_info = undefined;
+var cp_info = undefined;
+
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: '', cpName: '', cpRate: '',
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../firstTask/firstTask'
+    })
+  },
   onLoad: function (options) {
 
+    var that = this;
+
+    openid = app.globalData.openid
+
+    user_info = app.globalData.myData
+    var cp_openid = user_info.cp;
+
+    console.log(user_info)
+    console.log(cp_openid)
+
+    this.setData({
+      name: user_info.name,
+      cpRate: user_info.cp_rate
+    })
+
+    db.collection('user').where({
+      _openid: user_info.cp,
+    }).get({
+      success: res => {
+        console.log(res.data)
+        cp_info = res.data[0]
+
+        this.setData({
+          cpName: cp_info.name,
+        })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
