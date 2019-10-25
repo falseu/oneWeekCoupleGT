e450af7c2425db6d1b2fae34c63679b5ea2de99d
cccecilia <jliu738@gatech.edu>
2019-10-25 00:18:38 -0400

    instructions page

---
+++ miniprogram/pages/instructions/instructions.js
@@ -0,0 +1,22 @@
+//https://cloud.tencent.com/developer/article/1380912
+
+const db = wx.cloud.database();
+const cont = db.collection('user');
+const app = getApp()
+var openid = undefined;
+var user_info = undefined;
+
+Page({
+  
+  onLoad: function (options) {
+
+    var that = this;
+
+    openid = app.globalData.openid
+
+
+    
+  },
+
+  
+})
