bdcbe09b86157532939944bdc3aff87ff3317bec
cccecilia <jliu738@gatech.edu>
2019-10-24 23:02:34 -0400

    added new traits to user.info.display

--- miniprogram/pages/user_info_display/user_info_display.js
+++ miniprogram/pages/user_info_display/user_info_display.js
@@ -8,7 +8,7 @@ var user_info = undefined;
 
 Page({
   data: {
-    name: '', age: '', gender: '', height: '', weight: '', expectedGender: '', expectedAge: '', expectedHeight: '', expectedWeight: '', merits: [], expectedMerits: []
+    name: '', age: '', gender: '', height: '', weight: '', major: '', constellations: '', homeTown: '', hobbies: '', selfIntro: '', expectedGender: '', expectedAge: '', expectedHeight: '', expectedWeight: '', merits: [], expectedMerits: []
   },
 
   onLoad: function (options) {
@@ -23,6 +23,11 @@ Page({
       name: user_info.name,
       age: user_info.age,
       gender: user_info.gender,
+      major: user_info.major,
+      constellations: user_info.constellations,
+      homeTown: user_info.homeTown,
+      hobbies: user_info.hobbies,
+      selfIntro: user_info.selfIntro,
       height: user_info.height,
       weight: user_info.weight,
       expectedMerits: user_info.expectedMerits,
